373f1f842d0aea0dd1fa0490d083fa971b8367fa
MINJE-98 <jmj012100@gmail.com>
2022-09-08 22:49:38 +0900

    fix(seo): index페이지

--- library/seo/homeSeo.ts
+++ library/seo/homeSeo.ts
@@ -1,15 +1,13 @@
 import { DefaultSeoProps } from "next-seo";
 
-import { HOME_INTRO } from "@Constants/text";
-
 export const homeSeo: DefaultSeoProps = {
-  title: HOME_INTRO.TITLE,
-  description: HOME_INTRO.SUBTITLE,
+  title: "Today I Learned Log",
+  description: "오늘 배운것을 기록하고, 남들과 공유해보세요.",
   openGraph: {
     type: "website",
     locale: "ko_KR",
     url: "tilog.link",
-    title: HOME_INTRO.TITLE,
+    title: "Today I Learned Log",
     site_name: "TILog",
     images: [
       {
