6f776790e860101327f2a9a69cf6c93078c3c557
MINJE-98 <jmj012100@gmail.com>
2022-09-09 16:38:09 +0900

    fix(api-exception): ExceptionInterface

--- library/api/exception/interface/index.ts
+++ library/api/exception/interface/index.ts
@@ -6,14 +6,8 @@ type ExceptionMessageInterface = Partial<
   Record<typeof COUNTRY[keyof typeof COUNTRY], string>
 >;
 
-export interface ServerExceptionInterface {
-  readonly statusCode: number;
-  readonly requestLocation: string;
-  readonly message: ExceptionMessageInterface;
-}
-
 export interface ExceptionInterface {
   readonly statusCode: number;
   readonly requestLocation: string;
-  readonly message: String;
+  readonly message: ExceptionMessageInterface;
 }
