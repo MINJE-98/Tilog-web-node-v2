021d458e3bd99f63a2340d76bf9e8c729d7b8023
MINJE-98 <jmj012100@gmail.com>
2022-09-12 01:19:09 +0900

    feat: 게시글 수정 추가

---
+++ src/pages/editor.tsx
@@ -0,0 +1,88 @@
+import type { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+
+import { FormProvider, useForm } from "react-hook-form";
+
+import api from "@Api/index";
+import Header from "@Commons/organisms/header";
+import {
+  CoverImage,
+  PrivateSubmit,
+  PublicSubmit,
+  SearchCategory,
+  SubTitleInput,
+  TiptapEditor,
+  TitleInput,
+} from "@Components/writer";
+import withAuthServerSideProps from "@HOCS/withAuthGetServerSideProps";
+import useHandleSummit from "@Hooks/useHandleWriterSummit";
+import RootBox from "@Layouts/box/RootBox";
+
+import { GetPostDetailResponseDto } from "@til-log.lab/tilog-api";
+
+import WriterFormTypes from "@Components/writer/interface/writerFormTypes";
+
+interface EditorPageProps {
+  post: GetPostDetailResponseDto;
+}
+const EditorPage: NextPage<EditorPageProps> = ({ post }: EditorPageProps) => {
+  const method = useForm<WriterFormTypes>({
+    defaultValues: {
+      subTitle: post.subTitle,
+      thumbnailUrl: post.thumbnailUrl,
+      markdownContent: post.content,
+      categoryId: post.category.categoryId,
+      title: post.title,
+    },
+  });
+  const onSummit = useHandleSummit();
+  return (
+    <div>
+      <Head>
+        <title>글수정</title>
+      </Head>
+      <Header />
+      <RootBox>
+        <FormProvider {...method}>
+          <form onSubmit={method.handleSubmit(onSummit)}>
+            <CoverImage />
+            <TitleInput />
+            <SubTitleInput />
+            <SearchCategory />
+            <TiptapEditor />
+
+            <div className="flex w-[300px] sticky bottom-0 z-10 mt-3 space-x-5 ml-auto bg-slate-100">
+              <PrivateSubmit />
+              <PublicSubmit />
+            </div>
+          </form>
+        </FormProvider>
+      </RootBox>
+    </div>
+  );
+};
+
+export default EditorPage;
+export const getServerSideProps: GetServerSideProps = withAuthServerSideProps(
+  async (context) => {
+    const { postId } = context.query;
+    if (!postId) return { props: {} };
+    if (Array.isArray(postId)) return { props: {} };
+    try {
+      const { data } = await api.postService.getPostDetail(postId);
+      return {
+        props: {
+          post: data,
+        },
+      };
+    } catch (error) {
+      return {
+        notFound: true,
+      };
+    }
+  },
+  {
+    destination: "/",
+    permanent: true,
+  }
+);
