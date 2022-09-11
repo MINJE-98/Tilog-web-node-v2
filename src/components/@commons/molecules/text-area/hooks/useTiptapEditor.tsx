8e8ab8d7037969cd57b6a591e14245c358e582b7
MINJE-98 <jmj012100@gmail.com>
2022-09-12 01:20:23 +0900

    feat(molecules-text-area): tiptapEditor content 타입 안전 코드 추가

--- src/components/@commons/molecules/text-area/hooks/useTiptapEditor.tsx
+++ src/components/@commons/molecules/text-area/hooks/useTiptapEditor.tsx
@@ -1,20 +1,21 @@
 import Link from "@tiptap/extension-link";
-import { EditorOptions, useEditor } from "@tiptap/react";
+import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
 import { useFormContext } from "react-hook-form";
 
 import { WRITER_INPUT_TYPE } from "@Constants/input";
+import safeJsonParser from "@Utility/safeJsonParse";
 
 import { CreatePostRequestBodyDto } from "@til-log.lab/tilog-api";
 
-const useTiptapEditor = (content: EditorOptions["content"]) => {
-  const { setValue } = useFormContext<CreatePostRequestBodyDto>();
+const useTiptapEditor = () => {
+  const { setValue, getValues } = useFormContext<CreatePostRequestBodyDto>();
   return useEditor({
     extensions: [StarterKit, Link],
 
     editable: true,
-    content,
+    content: safeJsonParser(getValues("markdownContent")),
     onUpdate(props) {
       const updatedContent = JSON.stringify(props.editor.getJSON());
       setValue(WRITER_INPUT_TYPE.MARKDOWN_CONTENT, updatedContent);
