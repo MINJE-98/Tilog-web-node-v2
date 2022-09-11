639b523b2f61d71f23a4d90fa4643b23ab18a32f
MINJE-98 <jmj012100@gmail.com>
2022-09-11 16:48:53 +0900

    fix(_document): google 폰트 삭제

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,16 +6,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
 
         <body className="bg-neutral-50 dark:bg-neutral-900">
           <Main />
