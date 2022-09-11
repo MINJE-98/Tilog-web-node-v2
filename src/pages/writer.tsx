b51243a8cb9e614fb704ad47c12b240be6c46288
MINJE-98 <jmj012100@gmail.com>
2022-09-12 01:18:15 +0900

    fix(page-editor): Header추가, 게시글 발행 sticky옵션 추가

--- src/pages/writer.tsx
+++ src/pages/writer.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 
 import { FormProvider, useForm } from "react-hook-form";
 
+import Header from "@Commons/organisms/header";
 import {
   CoverImage,
   PrivateSubmit,
@@ -18,30 +19,32 @@ import RootBox from "@Layouts/box/RootBox";
 
 import WriterFormTypes from "@Components/writer/interface/writerFormTypes";
 
-// TODO: SEO
 const WriterPage: NextPage = () => {
   const method = useForm<WriterFormTypes>();
   const onSummit = useHandleSummit();
   return (
-    <RootBox>
+    <div>
       <Head>
         <title>글쓰기</title>
       </Head>
-      <FormProvider {...method}>
-        <form onSubmit={method.handleSubmit(onSummit)}>
-          <CoverImage />
-          <TitleInput />
-          <SubTitleInput />
-          <SearchCategory />
-          <TiptapEditor />
+      <Header />
+      <RootBox>
+        <FormProvider {...method}>
+          <form onSubmit={method.handleSubmit(onSummit)}>
+            <CoverImage />
+            <TitleInput />
+            <SubTitleInput />
+            <SearchCategory />
+            <TiptapEditor />
 
-          <div className="flex w-[300px] mt-3 space-x-5 ml-auto">
-            <PrivateSubmit />
-            <PublicSubmit />
-          </div>
-        </form>
-      </FormProvider>
-    </RootBox>
+            <div className="flex w-[300px] sticky bottom-0 z-10 mt-3 space-x-5 ml-auto bg-slate-100">
+              <PrivateSubmit />
+              <PublicSubmit />
+            </div>
+          </form>
+        </FormProvider>
+      </RootBox>
+    </div>
   );
 };
 
