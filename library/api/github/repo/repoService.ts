f2d0f2f58a1c283aab690fa06a10632c137821ea
MINJE-98 <jmj012100@gmail.com>
2022-09-16 16:49:06 +0900

    feat(api): github service axios private으로 변경

--- library/api/github/repo/repoService.ts
+++ library/api/github/repo/repoService.ts
@@ -5,7 +5,7 @@ import GithubRequest from "@Api/github/interface/githubRequest";
 import PinnedRepoResponse from "@Api/github/interface/pinnedRepoResponse";
 
 export default class RepoService {
-  constructor(readonly axios: AxiosInstance) {}
+  constructor(private readonly axios: AxiosInstance) {}
   getPinnedRepo(
     userName: GithubRequest["userName"]
   ): Promise<AxiosResponse<PinnedRepoResponse[], ExceptionInterface>> {
