8d2b12d4ed596fbdc174b5e26f3369a158a63114
MINJE-98 <jmj012100@gmail.com>
2022-09-16 16:59:34 +0900

    fix(layout-box): 시멘틱 테그로 변경

--- src/components/@layouts/box/RootBox.tsx
+++ src/components/@layouts/box/RootBox.tsx
@@ -3,9 +3,9 @@ import { HTMLAttributes } from "react";
 interface RootBoxProps extends HTMLAttributes<HTMLDivElement> {}
 
 const RootBox = ({ children, ...props }: RootBoxProps) => (
-  <div {...props} className="m-auto max-w-[1280px] items-center mt-20 px-5">
+  <main {...props} className="m-auto max-w-[1280px] items-center mt-20 px-5">
     {children}
-  </div>
+  </main>
 );
 
 export default RootBox;
