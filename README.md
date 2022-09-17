60e570b602e38cae19e5845ddbe237ae80cc3b4d
MINJE-98 <jmj012100@gmail.com>
2022-09-17 20:15:21 +0900

    feat: 테크 아이콘 추가, 색상코드 추가

--- README.md
+++ README.md
@@ -8,7 +8,31 @@ Blog platform for developers, TILog
 [Project Convention](https://github.com/TIL-Log-lab/Tilog-web-node-v2/discussions/3) •
 
 # 구조
-# 기능
+
+## 인프라
+
+## 폴더 구조
+
 # 컨벤션
+
+# 구현 범위
+
+## Home /
+
+## Popular /popular
+
+## Latest /latest
+
+## Blog /blog/:username
+
+## Post /post/:postId
+
+## Search /search?categoryId=:categoryId
+
+## Settings /settings
+
+## Writer /writer
+
+## Editor /editor?postId=:postId
 
 </div>
--- src/components/@commons/molecules/tech-icons/TechIcons.tsx
+++ src/components/@commons/molecules/tech-icons/TechIcons.tsx
@@ -1,8 +1,6 @@
 import { ReactElement } from "react";
 
 import * as BS from "react-icons/bs";
-import * as FA from "react-icons/fa";
-import * as GR from "react-icons/gr";
 import * as SI from "react-icons/si";
 
 interface TechIconsInterface {
@@ -13,19 +11,25 @@ const TechIcons: TechIconsInterface = {
   JAVASCRIPT: <SI.SiJavascript className="inline text-Javascript" />,
   TYPESCRIPT: <SI.SiTypescript className="inline text-Typescript" />,
   HTML: <SI.SiHtml5 className="inline text-HTML" />,
-  GITHUB: <SI.SiGithub className="inline" />,
   NESTJS: <SI.SiNestjs className="inline text-Nestjs" />,
   REACT: <SI.SiReact className="inline text-React" />,
-  SWIFT: <SI.SiSwift className="inline text-Swift" />,
-  DATABASE: <FA.FaDatabase className="inline text-DB" />,
-  MYSQL: <GR.GrMysql className="inline text-Mysql" />,
-  AWS: <SI.SiAmazonaws className="inline text-AWS" />,
-  DOCKER: <SI.SiDocker className="inline text-Docker" />,
-  DOCKER_COMPOSE: <SI.SiDocker className="inline text-Docker" />,
-  REDIS: <SI.SiRedis className="inline text-Redis " />,
-  NODEJS: <FA.FaNodeJs className="inline text-Nodejs" />,
   CSS: <SI.SiCss3 className="inline text-CSS" />,
-  INFRA: <BS.BsFillHddNetworkFill className="inline" />,
+  INFRA: (
+    <BS.BsFillHddNetworkFill className="inline text-neutral-800 dark:text-neutral-50" />
+  ),
+  PYTHON: <SI.SiPython className="inline text-Python" />,
+  JAVA: <SI.SiJava className="inline text-Java" />,
+  GO: <SI.SiGo className="inline text-Go" />,
+  CPP: <SI.SiCplusplus className="inline text-Cpp" />,
+  C: <SI.SiC className="inline text-C" />,
+  CSHARP: <SI.SiCsharp className="inline text-Csharp" />,
+  RUST: <SI.SiRust className="inline text-Rust" />,
+  SHELL: <SI.SiShell className="inline text-Shell" />,
+  KOTLIN: <SI.SiKotlin className="inline text-Kotlin" />,
+  RUBY: <SI.SiRuby className="inline text-Ruby" />,
+  PHP: <SI.SiPhp className="inline text-PHP" />,
+  SCALA: <SI.SiScala className="inline text-Scala" />,
+
   UNKNOWN: <BS.BsFillPatchQuestionFill className="inline" />,
 };
 export default TechIcons;
