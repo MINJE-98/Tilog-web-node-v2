162ec8f1b69d70e69a71a8f89e9aeba6bca4d8f6
MINJE-98 <jmj012100@gmail.com>
2022-09-17 15:16:50 +0900

    fix(categoryList): 카테고리 리스트가 없을 때 로직 수정

--- src/components/@commons/organisms/list/CategorySortButtonList.tsx
+++ src/components/@commons/organisms/list/CategorySortButtonList.tsx
@@ -3,14 +3,16 @@ import useGetAllCategories from "@Queries/categories/useGetAllCategories";
 
 const CategoryButtonList = () => {
   const categoryList = useGetAllCategories();
-  if (!categoryList.data) return <>없어</>;
+  if (!categoryList.data)
+    return <h2 className="text-base">카테고리가 존재하지 않아요.</h2>;
+
   return (
-    <div className="flex flex-wrap gap-3">
-      {categoryList.data.data.list.length === 0 && (
-        <h2 className="text-base">카테고리가 존재하지 않아요.</h2>
-      )}
+    <div className="flex flex-wrap gap-3 ">
       {categoryList.data.data.list.map((category) => (
-        <CategorySortButton categoryName={category.categoryName} />
+        <CategorySortButton
+          key={category.id}
+          categoryName={category.categoryName}
+        />
       ))}
     </div>
   );
