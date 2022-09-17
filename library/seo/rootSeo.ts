a798fb9a5590c2f1dfbeb3d6bbed757d52db1826
MINJE-98 <jmj012100@gmail.com>
2022-09-17 15:15:30 +0900

    fix(seo): root seo 변수로 수정

--- library/seo/rootSeo.ts
+++ library/seo/rootSeo.ts
@@ -1,33 +1,21 @@
-/* eslint-disable global-require */
-import { DefaultSeoProps } from "next-seo";
-
 import { HOME_INTRO } from "@Constants/text";
 
-import { GetPostsCategoryItem } from "@til-log.lab/tilog-api";
-
-export const rootSeo = (
-  category: GetPostsCategoryItem["name"]
-): DefaultSeoProps => {
-  return {
+export const rootSeo = {
+  title: HOME_INTRO.TITLE,
+  description: HOME_INTRO.SUBTITLE,
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "tilog.link",
     title: HOME_INTRO.TITLE,
-    description: HOME_INTRO.SUBTITLE,
-    openGraph: {
-      type: "website",
-      locale: "ko_KR",
-      url: "tilog.link",
-      title: HOME_INTRO.TITLE,
-      site_name: "TILog",
-      images: [
-        {
-          url:
-            category === ""
-              ? `https://og.tilog.link/.png?md=1&fontSize=90px&seed=3729&images=https://static.tilog.link/tilog-logo-white.svg`
-              : `https://og.tilog.link/.png?md=1&fontSize=90px&seed=3729&images=https://static.tilog.link/tilog-logo-white.svg&images=https://static.tilog.link/${category}.svg`,
-          width: 285,
-          height: 167,
-          alt: "TILog",
-        },
-      ],
-    },
-  };
+    site_name: "TILog",
+    images: [
+      {
+        url: "https://og.tilog.link/.png?md=1&fontSize=90px&seed=3729&images=https://static.tilog.link/tilog-logo-white.svg",
+        width: 285,
+        height: 167,
+        alt: "TILog",
+      },
+    ],
+  },
 };
