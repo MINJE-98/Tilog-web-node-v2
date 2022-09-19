968a4c0c564ceab9267b9c1c3d6eb4c7a7de1996
MINJE-98 <jmj012100@gmail.com>
2022-09-20 00:28:44 +0900

    fix(post): 컴포넌트 이름 변경

---
+++ src/components/post/PostAuthorDetail.tsx
@@ -0,0 +1,24 @@
+import UserBlogLink from "@Commons/molecules/link/UserBlogLink";
+import UserDetailProfile from "@Commons/molecules/profile/UserDetailProfile";
+import useGetUserProfile from "@Queries/users/useGetUserProfile";
+
+import { PostDetailUserInfoItem } from "@til-log.lab/tilog-api";
+
+interface PostAuthorDetailProps {
+  username: PostDetailUserInfoItem["username"];
+}
+
+const PostAuthorDetail = ({ username }: PostAuthorDetailProps) => {
+  const userInfo = useGetUserProfile(username);
+  if (!userInfo.data) return null;
+  return (
+    <div className="mt-20 rounded md:sticky md:top-4 md:mt-0 bg-neutral-100 dark:bg-neutral-800">
+      <div className="w-full p-5">
+        <UserDetailProfile userInfo={userInfo.data} />
+        <UserBlogLink userName={username} />
+      </div>
+    </div>
+  );
+};
+
+export default PostAuthorDetail;
--- src/components/post/PostHead.tsx
+++ src/components/post/PostHead.tsx
@@ -1,33 +1,21 @@
-import { Suspense } from "react";
-
-import { ErrorBoundary } from "react-error-boundary";
-
-import Spinner from "@Commons/atom/Spinner";
+import PostUpdateLink from "@Commons/molecules/link/PostUpdateLink";
 import UserProfile from "@Commons/molecules/profile/UserProfile";
 import RenderTechIcons from "@Commons/molecules/tech-icons/RenderTechIcons";
 import PostDeleteButton from "@Components/post/button/PostDeleteButton";
-import PostUpdateLink from "@Components/post/link/PostUpdateLink";
 import useIsOwner from "@Hooks/useIsOwner";
 
 import { GetPostDetailResponseDto } from "@til-log.lab/tilog-api";
 
-interface PostHeaderProps {
+interface PostHeadProps {
   post: GetPostDetailResponseDto;
 }
 
-const PostHeader = ({ post }: PostHeaderProps) => {
+const PostHead = ({ post }: PostHeadProps) => {
   const isOwner = useIsOwner(post.user.userId);
   return (
     <header>
       <div className="flex">
-        <Suspense fallback={<Spinner />}>
-          <ErrorBoundary fallback={<>에러 났어.</>}>
-            <UserProfile
-              userName={post.user.username}
-              avatar={post.user.avatar}
-            />
-          </ErrorBoundary>
-        </Suspense>
+        <UserProfile userName={post.user.username} avatar={post.user.avatar} />
         {isOwner && (
           <div className="flex ml-auto space-x-5">
             <PostDeleteButton postId={post.id} />
@@ -46,4 +34,4 @@ const PostHeader = ({ post }: PostHeaderProps) => {
   );
 };
 
-export default PostHeader;
+export default PostHead;
