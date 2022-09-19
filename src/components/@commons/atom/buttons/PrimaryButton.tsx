00a833eadf11fac295215b5fbfc0fae53baceee9
MINJE-98 <jmj012100@gmail.com>
2022-09-19 17:32:56 +0900

    fix(button): 버튼 타입은 상위 컴포넌트에서 결정합니다.

--- src/components/@commons/atom/buttons/PrimaryButton.tsx
+++ src/components/@commons/atom/buttons/PrimaryButton.tsx
@@ -1,18 +1,17 @@
+/** 버튼 타입은 상위 컴포넌트에서 결정합니다. */
+/* eslint-disable react/button-has-type */
+
 import { ButtonHTMLAttributes } from "react";
 
-interface PrimaryButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  isSubmit?: boolean;
-}
+interface PrimaryButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const PrimaryButton = ({
   children,
   className,
-  isSubmit,
   ...props
 }: PrimaryButtonProps) => {
   return (
     <button
-      type={!isSubmit ? "button" : "submit"}
       className={`${className} py-2.5 px-6 rounded-md w-full h-fit text-neutral-50 bg-neutral-800 dark:bg-black hover:bg-neutral-700`}
       {...props}
     >
