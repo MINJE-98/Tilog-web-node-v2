fcd53e08d4a88592091c3e15cfbaface1a3bdd1b
MINJE-98 <jmj012100@gmail.com>
2022-09-19 20:00:52 +0900

    fix(category): 선택된 카테고리와 그냥 카테고리 분리

---
+++ src/components/@commons/molecules/buttons/category/CategoryButton.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+import LinkTo from "@Commons/atom/LinkTo";
+
+interface CategoryButtonProps {
+  categoryName: string;
+}
+const CategoryButton = ({ categoryName }: CategoryButtonProps) => {
+  const router = useRouter();
+  return (
+    <LinkTo
+      href={{
+        pathname: router.pathname,
+        query: { ...router.query, category: categoryName },
+      }}
+      className="p-2 text-sm font-semibold rounded-sm hover:no-underline bg-neutral-50 dark:bg-neutral-800 ring-1 ring-neutral-200 dark:ring-neutral-700 hover:bg-neutral-100 hover:dark:bg-neutral-700"
+    >
+      <span className={`text-${categoryName}`}># </span>
+      <span className="text-neutral-900 dark:text-neutral-50">
+        {categoryName}
+      </span>
+    </LinkTo>
+  );
+};
+export default CategoryButton;
--- src/components/@commons/molecules/buttons/category/SelectedCategoryButton.tsx
+++ src/components/@commons/molecules/buttons/category/SelectedCategoryButton.tsx
@@ -8,26 +8,8 @@ interface CategorySortButtonProps {
 }
 const CategorySortButton = ({ categoryName }: CategorySortButtonProps) => {
   const router = useRouter();
-  if (router.query.category !== categoryName) {
-    return (
-      <LinkTo
-        scroll={false}
-        href={{
-          pathname: router.pathname,
-          query: { ...router.query, category: categoryName },
-        }}
-        className="p-2 text-sm font-semibold rounded-sm hover:no-underline bg-neutral-50 dark:bg-neutral-800 ring-1 ring-neutral-200 dark:ring-neutral-700 hover:bg-neutral-100 hover:dark:bg-neutral-700"
-      >
-        <span className={`text-${categoryName}`}># </span>
-        <span className="text-neutral-900 dark:text-neutral-50">
-          {categoryName}
-        </span>
-      </LinkTo>
-    );
-  }
   return (
     <LinkTo
-      scroll={false}
       href={`${router.asPath.split("?")[0]}`}
       className="p-2 text-sm font-semibold rounded-sm hover:no-underline bg-neutral-100 dark:bg-neutral-700 ring-1 ring-neutral-200 dark:ring-neutral-700 hover:bg-neutral-50 hover:dark:bg-neutral-800"
     >
