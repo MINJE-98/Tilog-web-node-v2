09981d6bf9907c2c1bbc2ab9b411c0eca686efc4
MINJE-98 <jmj012100@gmail.com>
2022-09-20 18:28:51 +0900

    feat(user): 인증된 유저 기본값 생성

---
+++ library/constants/user/defaultUserInfo.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_USER_INFO = {
+  id: 0,
+  name: "",
+  createdAt: "",
+  avatar: "",
+  settings: {
+    DISPLAY_NAME: "",
+    EMAIL: "",
+    POSITION: "",
+    INTRO_MSG: "",
+  },
+} as const;
