a1e06895315b907d23fc5ea8b73d3b801c2b4226
MINJE-98 <jmj012100@gmail.com>
2022-09-20 23:10:09 +0900

    feat(list): 비동기처리 로직 제거

--- src/components/home/list/LatestPostCardList.tsx
+++ src/components/home/list/LatestPostCardList.tsx
@@ -1,9 +1,5 @@
-import Spinner from "@Commons/atom/Spinner";
 import PostCard from "@Commons/molecules/card/post/PostCard";
-import More from "@Commons/molecules/link/More";
-import CardNavTitle from "@Commons/molecules/text/CardNavTitle";
 import PostCardList from "@Commons/organisms/list/PostCardList";
-import { ALL_LATEST_POST } from "@Constants/text";
 import useGetLatestPostList from "@Queries/posts/useGetLatestPostList";
 
 const LatestPostCardList = () => {
@@ -14,15 +10,7 @@ const LatestPostCardList = () => {
     maxContent: 6,
   });
   return (
-    <section className="my-10">
-      <CardNavTitle nav={<More href="latest" />}>
-        {ALL_LATEST_POST}
-      </CardNavTitle>
-      {latestPostList.isLoading && <Spinner />}
-      {latestPostList.isSuccess && (
-        <PostCardList CardComponent={PostCard} postList={latestPostList.data} />
-      )}
-    </section>
+    <PostCardList CardComponent={PostCard} postList={latestPostList.data} />
   );
 };
 
--- src/components/home/list/MostPopularPostCardList.tsx
+++ src/components/home/list/MostPopularPostCardList.tsx
@@ -1,9 +1,5 @@
-import Spinner from "@Commons/atom/Spinner";
 import MostPopularPostCard from "@Commons/molecules/card/post/MostPopularPostCard";
-import More from "@Commons/molecules/link/More";
-import CardNavTitle from "@Commons/molecules/text/CardNavTitle";
 import PostCardList from "@Commons/organisms/list/PostCardList";
-import { ALL_MOST_POPULAR_POST } from "@Constants/text";
 import useGetMostPopularPostList from "@Queries/posts/useGetMostPopularPostList";
 
 const MostPopularPostCardList = () => {
@@ -14,18 +10,10 @@ const MostPopularPostCardList = () => {
     maxContent: 6,
   });
   return (
-    <section className="my-10">
-      <CardNavTitle nav={<More href="popular" />}>
-        {ALL_MOST_POPULAR_POST}
-      </CardNavTitle>
-      {popularPostList.isLoading && <Spinner />}
-      {popularPostList.isSuccess && (
-        <PostCardList
-          CardComponent={MostPopularPostCard}
-          postList={popularPostList.data}
-        />
-      )}
-    </section>
+    <PostCardList
+      CardComponent={MostPopularPostCard}
+      postList={popularPostList.data}
+    />
   );
 };
 
