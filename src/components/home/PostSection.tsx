884044c24702e9a20675501693d8d6e0b7cb7b9a
MINJE-98 <jmj012100@gmail.com>
2022-09-20 23:10:38 +0900

    feat: suspense, errrboundary 적용

--- src/components/home/PostSection.tsx
+++ src/components/home/PostSection.tsx
@@ -1,12 +1,59 @@
-import LatestPostCardList from "@Components/home/list/LatestPostCardList";
-import MostPopularPostCardList from "@Components/home/list/MostPopularPostCardList";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+import { ErrorBoundary } from "react-error-boundary";
+import { QueryErrorResetBoundary } from "react-query";
+
+import Spinner from "@Commons/atom/Spinner";
+import ComponentLoadError from "@Commons/molecules/ComponentLoadError";
+import More from "@Commons/molecules/link/More";
+import CardNavTitle from "@Commons/molecules/text/CardNavTitle";
+import { ALL_LATEST_POST, ALL_MOST_POPULAR_POST } from "@Constants/text";
+
+const LatestPostCardList = dynamic(
+  () => import("@Components/home/list/LatestPostCardList"),
+  { suspense: true }
+);
+
+const MostPopularPostCardList = dynamic(
+  () => import("@Components/home/list/MostPopularPostCardList"),
+  { suspense: true }
+);
 
 const PostSection = () => {
   return (
-    <>
-      <MostPopularPostCardList />
-      <LatestPostCardList />
-    </>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <>
+          <section className="my-10">
+            <CardNavTitle nav={<More href="latest" />}>
+              {ALL_LATEST_POST}
+            </CardNavTitle>
+            <Suspense fallback={<Spinner />}>
+              <ErrorBoundary
+                onReset={reset}
+                fallbackRender={ComponentLoadError}
+              >
+                <MostPopularPostCardList />
+              </ErrorBoundary>
+            </Suspense>
+          </section>
+          <section className="my-10">
+            <CardNavTitle nav={<More href="popular" />}>
+              {ALL_MOST_POPULAR_POST}
+            </CardNavTitle>
+            <Suspense fallback={<Spinner />}>
+              <ErrorBoundary
+                onReset={reset}
+                fallbackRender={ComponentLoadError}
+              >
+                <LatestPostCardList />
+              </ErrorBoundary>
+            </Suspense>
+          </section>
+        </>
+      )}
+    </QueryErrorResetBoundary>
   );
 };
 
