0ff9744db3c7642276c158b229eff9f54144df90
MINJE-98 <jmj012100@gmail.com>
2022-09-20 13:42:59 +0900

    feat(postDetail): error 시 페이지 반환 로직 추가

--- library/hooks/queries/posts/useGetPostDetail.ts
+++ library/hooks/queries/posts/useGetPostDetail.ts
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+
 import { useQuery } from "react-query";
 
 import api from "@Api";
@@ -6,13 +8,19 @@ import { postQueryKeys } from "@Utility/queryKey";
 import { Posts } from "@Api/interface/model";
 
 const useGetPostDetail = (postId: Posts["id"]) => {
+  const router = useRouter();
   return useQuery(
     postQueryKeys.detailPostId(postId),
     () => api.postService.getPostDetail(postId),
     {
+      enabled: !!postId,
       retry: 0,
       refetchOnWindowFocus: false,
       refetchInterval: false,
+      suspense: true,
+      onError: () => {
+        router.push("/404");
+      },
     }
   );
 };
