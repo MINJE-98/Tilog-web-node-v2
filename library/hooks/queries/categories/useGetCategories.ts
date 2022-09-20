6c4574fb550791a5090cd963abaf94bb9e0f0d34
MINJE-98 <jmj012100@gmail.com>
2022-09-20 16:17:34 +0900

    ifx(categories): 카테고리 검색 훅 삭제 후 하나로 통합

---
+++ library/hooks/queries/categories/useGetCategories.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "react-query";
+
+import api from "@Api";
+import { categoryQueryKeys } from "@Utility/queryKey";
+
+import { Category } from "@Api/interface/model";
+
+const useGetCategories = (categoryName: Category["categoryName"]) => {
+  return useQuery(
+    categoryQueryKeys.categoryName(categoryName),
+    () => api.categoryService.getCategories(categoryName),
+    {
+      retry: 0,
+      refetchOnWindowFocus: false,
+      refetchInterval: false,
+      retryOnMount: false,
+    }
+  );
+};
+
+export default useGetCategories;
