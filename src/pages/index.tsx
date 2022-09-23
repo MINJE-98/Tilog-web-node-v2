cd504706f4564f6a90845bf3b45e9a6dceb2b4d6
MINJE-98 <jmj012100@gmail.com>
2022-09-23 15:56:05 +0900

    feat: index페이지에 suspense, ErrorBoundary 제거, 다이나믹 import 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,22 @@
 import type { GetServerSideProps, NextPage } from "next";
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
 
 import { DefaultSeo } from "next-seo";
-import { ErrorBoundary } from "react-error-boundary";
-import {
-  dehydrate,
-  QueryClient,
-  useQueryErrorResetBoundary,
-} from "react-query";
+import { dehydrate, QueryClient } from "react-query";
 
 import api from "@Api/index";
-import Spinner from "@Commons/atom/Spinner";
-import ComponentLoadError from "@Commons/molecules/ComponentLoadError";
 import More from "@Commons/molecules/link/More";
 import CardNavTitle from "@Commons/molecules/text/CardNavTitle";
 import IntroThumbnail from "@Components/home/IntroThumbnail";
-// import LatestPostCardList from "@Components/home/list/LatestPostCardList";
-// import MostPopularPostCardList from "@Components/home/list/MostPopularPostCardList";
+import LatestPostCardList from "@Components/home/list/LatestPostCardList";
+import MostPopularPostCardList from "@Components/home/list/MostPopularPostCardList";
 import { ALL_LATEST_POST, ALL_MOST_POPULAR_POST } from "@Constants/text";
 import RootBox from "@Layouts/box/RootBox";
 import { rootSeo } from "@SEO";
 import { postQueryKeys } from "@Utility/queryKey";
 
-const LatestPostCardList = dynamic(
-  () => import("@Components/home/list/LatestPostCardList"),
-  {
-    suspense: true,
-  }
-);
-
-const MostPopularPostCardList = dynamic(
-  () => import("@Components/home/list/MostPopularPostCardList"),
-  {
-    suspense: true,
-  }
-);
 const Home: NextPage = () => {
-  const { reset } = useQueryErrorResetBoundary();
   return (
-    <>
+    <div>
       <DefaultSeo {...rootSeo} />
       <IntroThumbnail />
       <RootBox>
@@ -47,24 +24,16 @@ const Home: NextPage = () => {
           <CardNavTitle nav={<More href="popular" />}>
             {ALL_MOST_POPULAR_POST}
           </CardNavTitle>
-          <Suspense fallback={<Spinner />}>
-            <ErrorBoundary onReset={reset} fallbackRender={ComponentLoadError}>
-              <MostPopularPostCardList />
-            </ErrorBoundary>
-          </Suspense>
+          <MostPopularPostCardList />
         </section>
         <section className="my-10">
           <CardNavTitle nav={<More href="latest" />}>
             {ALL_LATEST_POST}
           </CardNavTitle>
-          <Suspense fallback={<Spinner />}>
-            <ErrorBoundary onReset={reset} fallbackRender={ComponentLoadError}>
-              <LatestPostCardList />
-            </ErrorBoundary>
-          </Suspense>
+          <LatestPostCardList />
         </section>
       </RootBox>
-    </>
+    </div>
   );
 };
 export default Home;
@@ -77,7 +46,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     () =>
       api.postService.getPosts({
         dateScope: "All",
-        sortScope: "createdAt",
+        sortScope: "likes",
         page: 0,
         maxContent: 6,
       })
@@ -85,7 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   await queryClient.prefetchQuery(postQueryKeys.postListLatest(), () =>
     api.postService.getPosts({
       dateScope: "All",
-      sortScope: "likes",
+      sortScope: "createdAt",
       page: 0,
       maxContent: 6,
     })
