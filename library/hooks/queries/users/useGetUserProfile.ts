60eb2f8d96353f2967374640b190b8d9c6829a19
MINJE-98 <jmj012100@gmail.com>
2022-09-23 23:16:31 +0900

    fix(users): suspense, errorboundary 적용

--- library/hooks/queries/users/useGetUserProfile.ts
+++ library/hooks/queries/users/useGetUserProfile.ts
@@ -15,6 +15,8 @@ export default function useGetUserProfile(userName: Users["userName"]) {
       refetchInterval: false,
       retryOnMount: false,
       refetchOnMount: false,
+      suspense: true,
+      useErrorBoundary: true,
     }
   );
 }
