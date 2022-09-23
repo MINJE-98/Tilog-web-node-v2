7e8e3fa423b547152b27cac6c7b9f37ef2c62afa
MINJE-98 <jmj012100@gmail.com>
2022-09-24 00:08:11 +0900

    feat: postDetail 하위 컴포넌트 생성

---
+++ src/components/post/index.tsx
@@ -0,0 +1,38 @@
+import PostThumbnailImage from "@Commons/molecules/images/PostThumbnailImage";
+import TiptapViewer from "@Commons/molecules/text-area/TiptapViewer";
+import PostAuthorDetail from "@Components/post/PostAuthorDetail";
+import PostHead from "@Components/post/PostHead";
+import PostLike from "@Components/post/PostLike";
+import withSuspenseAndErrorBoundary from "@HOCS/withSuspenseAndErrorBoundary";
+import useGetPostDetail from "@Queries/posts/useGetPostDetail";
+
+import { GetPostDetailResponseDto } from "@til-log.lab/tilog-api";
+
+const PostDetail = ({ post }: { post: GetPostDetailResponseDto }) => {
+  const { data } = useGetPostDetail(post.id);
+  if (!data) return null;
+  return (
+    <article className="w-full">
+      <PostHead post={data} />
+      <PostThumbnailImage
+        id={data.id}
+        thumbnailUrl={data.thumbnailUrl}
+        title={data.title}
+      />
+
+      <div className="mt-5 lg:grid lg:grid-flow-col lg:space-x-5 lg:grid-cols-3">
+        <article className="lg:col-span-2">
+          <TiptapViewer content={JSON.parse(data.content)} />
+        </article>
+        <aside>
+          <PostAuthorDetail username={data.user.username} />
+        </aside>
+      </div>
+      <section className="mt-5">
+        <PostLike postId={data.id} count={data.like} />
+      </section>
+    </article>
+  );
+};
+
+export default withSuspenseAndErrorBoundary(PostDetail);
