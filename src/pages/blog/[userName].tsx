ccd98d9d5c5c3956572ba2d6e771785498639cf0
MINJE-98 <jmj012100@gmail.com>
2022-09-23 21:29:07 +0900

    feat: 서버측 렌더링 적용

--- src/pages/blog/[userName].tsx
+++ src/pages/blog/[userName].tsx
@@ -1,22 +1,28 @@
 import { GetServerSideProps, NextPage } from "next";
 
 import { DefaultSeo } from "next-seo";
+import { dehydrate, QueryClient } from "react-query";
 
 import api from "@Api";
-import BlogPostSection from "@Components/blog/BlogPostSection";
+import CardNavTitle from "@Commons/molecules/text/CardNavTitle";
+import BlogPostList from "@Components/blog/list/BlogPostList";
 import UserStatsSection from "@Components/blog/UserStatsSection";
+import withAuthServerSideProps from "@HOCS/withAuthGetServerSideProps";
 import BlogBox from "@Layouts/box/BlogBox";
 import RootBox from "@Layouts/box/RootBox";
 import { userBlogDetailSeo } from "@SEO";
+import { categoryQueryKeys, postQueryKeys } from "@Utility/queryKey";
 
 import GetUserProfileResponse from "@Api/users/interface/getUserProfileResponse";
 
 interface BlogPagePageProps {
   userInfo: GetUserProfileResponse;
+  categoryName: string;
 }
 
 const BlogPage: NextPage<BlogPagePageProps> = ({
   userInfo,
+  categoryName,
 }: BlogPagePageProps) => {
   const seo = userBlogDetailSeo(userInfo);
 
@@ -25,26 +31,56 @@ const BlogPage: NextPage<BlogPagePageProps> = ({
       <DefaultSeo {...seo} />
       <BlogBox>
         <UserStatsSection userInfo={userInfo} />
-        <BlogPostSection userId={userInfo.id} />
+        <div className="min-w-[600px] w-full mt-10 md:mt-0">
+          <CardNavTitle>{categoryName} Posts</CardNavTitle>
+          <BlogPostList categoryName={categoryName} userId={userInfo.id} />
+        </div>
       </BlogBox>
     </RootBox>
   );
 };
 export default BlogPage;
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { userName } = context.query;
-  if (!userName) return { notFound: true };
-  if (Array.isArray(userName)) return { notFound: true };
-  try {
-    const userInfo = await api.usersService.getUserProfile(userName);
+export const getServerSideProps: GetServerSideProps = withAuthServerSideProps(
+  async (context) => {
+    const { userName, category } = context.query;
+    const categoryName = !category ? "" : category;
+    if (!userName) return { notFound: true };
+
+    if (Array.isArray(userName) || Array.isArray(categoryName))
+      return { notFound: true };
+
+    const queryClient = new QueryClient();
+    let userInfo: GetUserProfileResponse;
+    try {
+      userInfo = await api.usersService.getUserProfile(userName);
+    } catch (error) {
+      return {
+        notFound: true,
+      };
+    }
+
+    await queryClient.prefetchInfiniteQuery(
+      postQueryKeys.postListInfiniteUserCategoryName(userInfo.id, ""),
+      () =>
+        api.postService.getCategoryPosts({
+          dateScope: "All",
+          sortScope: "createdAt",
+          page: 0,
+          categoryName,
+          maxContent: 10,
+          userId: userInfo.id,
+        })
+    );
+    await queryClient.prefetchQuery(categoryQueryKeys.categoryName(), () =>
+      api.categoryService.getCategories()
+    );
+
     return {
       props: {
+        dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
         userInfo,
+        categoryName,
       },
     };
-  } catch (error) {
-    return {
-      notFound: true,
-    };
   }
-};
+);
