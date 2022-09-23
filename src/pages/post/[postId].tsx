8ff6bca80cd0e33bc2114f3727ff82333a725ff1
MINJE-98 <jmj012100@gmail.com>
2022-09-24 00:08:21 +0900

    feat: 서버사이드 렌더링 추가

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -1,19 +1,21 @@
 import { GetServerSideProps, NextPage } from "next";
+import dynamic from "next/dynamic";
 
 import { DefaultSeo } from "next-seo";
+import { dehydrate, QueryClient } from "react-query";
 
 import api from "@Api/index";
-import PostThumbnailImage from "@Commons/molecules/images/PostThumbnailImage";
-import TiptapViewer from "@Commons/molecules/text-area/TiptapViewer";
-import Comment from "@Components/comment";
-import PostAuthorDetail from "@Components/post/PostAuthorDetail";
-import PostHead from "@Components/post/PostHead";
-import PostLike from "@Components/post/PostLike";
+import PostDetail from "@Components/post";
 import withAuthServerSideProps from "@HOCS/withAuthGetServerSideProps";
 import RootBox from "@Layouts/box/RootBox";
 import { postDetailSeo } from "@SEO";
+import { postQueryKeys, userQueryKeys } from "@Utility/queryKey";
 
 import { GetPostDetailResponseDto } from "@til-log.lab/tilog-api";
+
+const Comment = dynamic(() => import("@Components/comment"), {
+  ssr: false,
+});
 
 interface PostDetailPageProps {
   post: GetPostDetailResponseDto;
@@ -22,32 +24,13 @@ const PostDetailPage: NextPage<PostDetailPageProps> = ({
   post,
 }: PostDetailPageProps) => {
   const seo = postDetailSeo(post);
+
   return (
     <div>
       <DefaultSeo {...seo} />
       <RootBox>
-        <article className="w-full">
-          <PostHead post={post} />
-          <figure>
-            <PostThumbnailImage
-              id={post.id}
-              thumbnailUrl={post.thumbnailUrl}
-              title={post.title}
-            />
-          </figure>
-          <div className="mt-5 lg:grid lg:grid-flow-col lg:space-x-5 lg:grid-cols-3">
-            <article className="lg:col-span-2">
-              <TiptapViewer content={JSON.parse(post.content)} />
-            </article>
-            <aside>
-              <PostAuthorDetail username={post.user.username} />
-            </aside>
-          </div>
-        </article>
-        <section className="mt-5">
-          <PostLike postId={post.id} count={post.like} />
-          <Comment postId={post.id} />
-        </section>
+        <PostDetail post={post} />
+        <Comment postId={post.id} />
       </RootBox>
     </div>
   );
@@ -59,17 +42,27 @@ export const getServerSideProps: GetServerSideProps = withAuthServerSideProps(
     const { postId } = context.query;
     if (!postId) return { props: {} };
     if (Array.isArray(postId)) return { props: {} };
+    let post: GetPostDetailResponseDto;
     try {
-      const { data } = await api.postService.getPostDetail(postId);
-      return {
-        props: {
-          post: data,
-        },
-      };
+      post = await api.postService.getPostDetail(postId);
     } catch (error) {
       return {
         notFound: true,
       };
     }
+
+    const queryClient = new QueryClient();
+    await queryClient.setQueryData(postQueryKeys.detailPostId(postId), post);
+    await queryClient.prefetchQuery(
+      userQueryKeys.userDetailUserName(post.user.username),
+      () => api.usersService.getUserProfile(post.user.username)
+    );
+
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+        post,
+      },
+    };
   }
 );
