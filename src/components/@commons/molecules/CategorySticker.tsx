0a6e300765c4ad4ce99970d73020efc87bc10bfd
MINJE-98 <jmj012100@gmail.com>
2022-10-02 22:15:37 +0900

    feat: isSelected, dark 모드 추가

--- src/components/@commons/molecules/CategorySticker.tsx
+++ src/components/@commons/molecules/CategorySticker.tsx
@@ -1,20 +1,33 @@
-// 카테고리의 아이콘이랑 이름을 사용해서 스티커 형식으로 컴포넌트에 붙여쓰기 위한 컴포넌트이다.
-
 import RenderTechIcons from "@Commons/molecules/tech-icons/RenderTechIcons";
 
 import { GetPostsCategoryItem } from "@til-log.lab/tilog-api";
 
+interface CategoryStickerProps {
+  categoryName: GetPostsCategoryItem["name"];
+  isSelected?: boolean;
+}
 const CategorySticker = ({
   categoryName,
-}: {
-  categoryName: GetPostsCategoryItem["name"];
-}) => {
+  isSelected = false,
+}: CategoryStickerProps) => {
   return (
-    <div className="flex items-center pt-[2px] pb-[4px] px-4 space-x-2 bg-neutral-800 rounded-2xl w-fit">
+    <div
+      className={`${
+        isSelected
+          ? "bg-neutral-700 dark:bg-neutral-200"
+          : "bg-neutral-200 dark:bg-neutral-700"
+      }  flex items-center pt-[2px] pb-[4px] px-4 space-x-2 rounded-2xl w-fit`}
+    >
       <div className="text-xl">
         <RenderTechIcons categoryName={categoryName} />
       </div>
-      <span className="mt-[3px] font-semibold text-neutral-50">
+      <span
+        className={`${
+          isSelected
+            ? "text-neutral-50 dark:text-neutral-800"
+            : "text-neutral-800 dark:text-neutral-50"
+        } mt-[2px] font-semibold`}
+      >
         {categoryName}
       </span>
     </div>
