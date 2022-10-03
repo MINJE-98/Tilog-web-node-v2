7e0a88ba633c5e0411cb5c9a3e4d5ff1a18fef6d
MINJE-98 <jmj012100@gmail.com>
2022-10-03 21:12:25 +0900

    fix: 사용하지 않는 기능 제거

--- src/components/@models/home/list/MostPopularSwiper.tsx
+++ src/components/@models/home/list/MostPopularSwiper.tsx
@@ -1,5 +1,5 @@
 import { useMediaQuery } from "react-responsive";
-import { A11y, Autoplay, Pagination, Parallax } from "swiper";
+import { A11y, Autoplay, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import LinkTo from "@Commons/atom/LinkTo";
@@ -17,48 +17,50 @@ const MostPopularSwiper = () => {
   });
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 767px)" });
   return (
-    <div className="max-w-[310px] sm:max-w-[500px] md:max-w-[700px]">
-      <Swiper
-        modules={[Pagination, A11y, Autoplay, Parallax]}
-        slidesPerView={1}
-        spaceBetween={20}
-        loopAdditionalSlides={1}
-        style={{
-          overflow: "visible",
-        }}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true,
-          stopOnLastSlide: false,
-          reverseDirection: false,
-        }}
-        loop
-        speed={1000}
-        loopedSlides={1}
-        parallax
-        pagination={{
-          type: "fraction",
-          renderFraction: (currentClass, totalClass) =>
-            `<div class="tilog-pagination">
+    <div className="flex justify-center">
+      <div className="max-w-[310px] sm:max-w-[500px] md:max-w-[700px]">
+        <Swiper
+          modules={[Pagination, A11y, Autoplay]}
+          slidesPerView={1}
+          spaceBetween={20}
+          loopAdditionalSlides={1}
+          style={{
+            overflow: "visible",
+          }}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+            stopOnLastSlide: false,
+            reverseDirection: false,
+          }}
+          loop
+          speed={1000}
+          loopedSlides={1}
+          parallax
+          pagination={{
+            type: "fraction",
+            renderFraction: (currentClass, totalClass) =>
+              `<div class="tilog-pagination">
         <span class="${currentClass}"></span> / <span class="${totalClass}"></span>
         </div>`,
-        }}
-      >
-        {popularPostList.data?.list.map((post) => (
-          <SwiperSlide key={post.id}>
-            {({ isActive }) => (
-              <LinkTo href={`post/${post.id}`} className="hover:no-underline">
-                {isTabletOrMobile ? (
-                  <SwipeMobileCard isActive={isActive} post={post} />
-                ) : (
-                  <SwipeCard isActive={isActive} post={post} />
-                )}
-              </LinkTo>
-            )}
-          </SwiperSlide>
-        ))}
-      </Swiper>
+          }}
+        >
+          {popularPostList.data?.list.map((post) => (
+            <SwiperSlide key={post.id}>
+              {({ isActive }) => (
+                <LinkTo href={`post/${post.id}`} className="hover:no-underline">
+                  {isTabletOrMobile ? (
+                    <SwipeMobileCard isActive={isActive} post={post} />
+                  ) : (
+                    <SwipeCard isActive={isActive} post={post} />
+                  )}
+                </LinkTo>
+              )}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
