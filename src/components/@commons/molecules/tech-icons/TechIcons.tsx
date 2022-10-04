802a0b112e77138d47b8a48a33c7c273fd56e0d4
MINJE-98 <jmj012100@gmail.com>
2022-10-04 20:41:36 +0900

    feat: add BOOKSTUDY, TIL Icon

--- src/components/@commons/molecules/tech-icons/TechIcons.tsx
+++ src/components/@commons/molecules/tech-icons/TechIcons.tsx
@@ -1,8 +1,29 @@
 import { ReactElement } from "react";
 
 import { BsQuestionCircleFill } from "react-icons/bs";
+import { GiBookCover } from "react-icons/gi";
 import { GrServerCluster } from "react-icons/gr";
-import * as SI from "react-icons/si";
+import {
+  SiC,
+  SiCplusplus,
+  SiCsharp,
+  SiCss3,
+  SiGo,
+  SiHtml5,
+  SiJava,
+  SiJavascript,
+  SiKotlin,
+  SiNestjs,
+  SiPhp,
+  SiPython,
+  SiReact,
+  SiRuby,
+  SiRust,
+  SiScala,
+  SiShell,
+  SiSolidity,
+  SiTypescript,
+} from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 
 interface TechIconsInterface {
@@ -10,27 +31,29 @@ interface TechIconsInterface {
 }
 
 const TechIcons: TechIconsInterface = {
-  JAVASCRIPT: <SI.SiJavascript className="inline text-Javascript" />,
-  TYPESCRIPT: <SI.SiTypescript className="inline text-Typescript" />,
-  HTML: <SI.SiHtml5 className="inline text-HTML" />,
-  NESTJS: <SI.SiNestjs className="inline text-Nestjs" />,
-  REACT: <SI.SiReact className="inline text-React" />,
-  CSS: <SI.SiCss3 className="inline text-CSS" />,
+  JAVASCRIPT: <SiJavascript className="inline text-Javascript" />,
+  TYPESCRIPT: <SiTypescript className="inline text-Typescript" />,
+  HTML: <SiHtml5 className="inline text-HTML" />,
+  NESTJS: <SiNestjs className="inline text-Nestjs" />,
+  REACT: <SiReact className="inline text-React" />,
+  CSS: <SiCss3 className="inline text-CSS" />,
   INFRA: <GrServerCluster className="inline text-neutral-50" />,
-  PYTHON: <SI.SiPython className="inline text-Python" />,
-  JAVA: <SI.SiJava className="inline text-Java" />,
-  GO: <SI.SiGo className="inline text-Go" />,
-  CPP: <SI.SiCplusplus className="inline text-Cpp" />,
-  C: <SI.SiC className="inline text-C" />,
-  CSHARP: <SI.SiCsharp className="inline text-Csharp" />,
-  RUST: <SI.SiRust className="inline text-Rust" />,
-  SHELL: <SI.SiShell className="inline text-Shell" />,
-  KOTLIN: <SI.SiKotlin className="inline text-Kotlin" />,
-  RUBY: <SI.SiRuby className="inline text-Ruby" />,
-  PHP: <SI.SiPhp className="inline text-PHP" />,
-  SCALA: <SI.SiScala className="inline text-Scala" />,
-  SOLIDITY: <SI.SiSolidity className="inline text-Ruby" />,
+  PYTHON: <SiPython className="inline text-Python" />,
+  JAVA: <SiJava className="inline text-Java" />,
+  GO: <SiGo className="inline text-Go" />,
+  CPP: <SiCplusplus className="inline text-Cpp" />,
+  C: <SiC className="inline text-C" />,
+  CSHARP: <SiCsharp className="inline text-Csharp" />,
+  RUST: <SiRust className="inline text-Rust" />,
+  SHELL: <SiShell className="inline text-Shell" />,
+  KOTLIN: <SiKotlin className="inline text-Kotlin" />,
+  RUBY: <SiRuby className="inline text-Ruby" />,
+  PHP: <SiPhp className="inline text-PHP" />,
+  SCALA: <SiScala className="inline text-Scala" />,
+  SOLIDITY: <SiSolidity className="inline text-Ruby" />,
   NEXTJS: <TbBrandNextjs className="inline text-neutral-50" />,
+  BOOKSTUDY: <GiBookCover className="inline text-neutral-50" />,
+  TIL: <>✍🏼</>,
   UNKNOWN: <BsQuestionCircleFill className="inline text-neutral-50" />,
 };
 export default TechIcons;
