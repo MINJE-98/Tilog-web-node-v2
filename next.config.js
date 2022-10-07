82778420663cc3fbb2d6c18378bff3bb5daff652
MINJE-98 <jmj012100@gmail.com>
2022-10-07 21:23:30 +0900

    feat: 모듈 분석 모듈 추가

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 const nextConfig = {
   reactStrictMode: true,
   env: {
@@ -12,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
