4bab717bb33f2fff54b513587c0880a5f994576c
MINJE-98 <jmj012100@gmail.com>
2022-10-07 21:22:49 +0900

    fix: 더이상 config에서 joi가 적용되지 않습니다.

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
 /** @type {import('next').NextConfig} */
-const Joi = require("joi");
-const schema = Joi.object({
-  NEXT_PUBLIC_TILOG_API: Joi.string().required(),
-  NEXT_PUBLIC_TILOG_AUTH: Joi.string().required(),
-});
 
-(async function () {
-  try {
-    await schema.validateAsync({
-      NEXT_PUBLIC_TILOG_API: process.env.NEXT_PUBLIC_TILOG_API,
-      NEXT_PUBLIC_TILOG_AUTH: process.env.NEXT_PUBLIC_TILOG_AUTH,
-      NEXT_PUBLIC_TILOG_GITHUB_STATS_API:
-        process.env.NEXT_PUBLIC_TILOG_GITHUB_STATS_API,
-    });
-  } catch (e) {
-    throw Error(e);
-  }
-})();
-console.log(process.env.NEXT_PUBLIC_TILOG_API);
-console.log(process.env.NEXT_PUBLIC_TILOG_AUTH);
-console.log(process.env.NEXT_PUBLIC_TILOG_GITHUB_STATS_API);
 const nextConfig = {
   reactStrictMode: true,
   env: {
