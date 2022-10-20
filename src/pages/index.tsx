782dc16c85b81c3b61732127d185b92bd4bba91d
MINJE-98 <jmj012100@gmail.com>
2022-10-20 16:17:33 +0900

    feat: idnex페이지 정적페이지로 변환

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import type { GetStaticProps, NextPage } from "next";
+import type { NextPage } from "next";
 
 import { DefaultSeo } from "next-seo";
-import { dehydrate, QueryClient } from "react-query";
+// import { dehydrate, QueryClient } from "react-query";
 
-import api from "@Api/index";
+// import api from "@Api/index";
 import RootBox from "@Layouts/box/RootBox";
 import IntroThumbnail from "@Models/home/IntroThumbnail";
 import LatestPostSection from "@Models/home/LatestPostSection";
 import MostPopularPostSection from "@Models/home/MostPopularPostSection";
 import { rootSeo } from "@SEO";
-import { postQueryKeys } from "@Utility/queryKey";
+// import { postQueryKeys } from "@Utility/queryKey";
 
 const Home: NextPage = () => {
   return (
@@ -25,35 +25,35 @@ const Home: NextPage = () => {
 };
 export default Home;
 
-export const getStaticProps: GetStaticProps = async () => {
-  const queryClient = new QueryClient();
-  Promise.all([
-    await queryClient.prefetchQuery(
-      postQueryKeys.postListPopularDateScope("All"),
-      () =>
-        api.postService.getPosts({
-          dateScope: "All",
-          sortScope: "likes",
-          page: 0,
-          maxContent: 6,
-        })
-    ),
-    await queryClient.prefetchInfiniteQuery(
-      postQueryKeys.postListInfinitePopularDateScope("All"),
-      () =>
-        api.postService.getPosts({
-          dateScope: "All",
-          sortScope: "createdAt",
-          page: 0,
-          maxContent: 6,
-        })
-    ),
-  ]);
+// export const getStaticProps: GetStaticProps = async () => {
+//   const queryClient = new QueryClient();
+//   Promise.all([
+//     await queryClient.prefetchQuery(
+//       postQueryKeys.postListPopularDateScope("All"),
+//       () =>
+//         api.postService.getPosts({
+//           dateScope: "All",
+//           sortScope: "likes",
+//           page: 0,
+//           maxContent: 6,
+//         })
+//     ),
+//     await queryClient.prefetchInfiniteQuery(
+//       postQueryKeys.postListInfinitePopularDateScope("All"),
+//       () =>
+//         api.postService.getPosts({
+//           dateScope: "All",
+//           sortScope: "createdAt",
+//           page: 0,
+//           maxContent: 6,
+//         })
+//     ),
+//   ]);
 
-  return {
-    props: {
-      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-    },
-    revalidate: 30,
-  };
-};
+//   return {
+//     props: {
+//       dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+//     },
+//     revalidate: 30,
+//   };
+// };
