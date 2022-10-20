920e0119850ad9436d8c0fb049ad940c806423de
MINJE-98 <jmj012100@gmail.com>
2022-10-20 16:35:37 +0900

    fix: remove ssr

--- src/pages/popular.tsx
+++ src/pages/popular.tsx
@@ -1,13 +1,10 @@
-import { GetStaticProps, NextPage } from "next";
+import { NextPage } from "next";
 
 import { popularSeo } from "library/seo/popularSeo";
 import { DefaultSeo } from "next-seo";
-import { dehydrate, QueryClient } from "react-query";
 
-import api from "@Api/index";
 import RootBox from "@Layouts/box/RootBox";
 import PopularPostSection from "@Models/popular/PopularPostSection";
-import { postQueryKeys } from "@Utility/queryKey";
 
 const PopularPage: NextPage = () => {
   return (
@@ -21,22 +18,3 @@ const PopularPage: NextPage = () => {
 };
 
 export default PopularPage;
-export const getStaticProps: GetStaticProps = async () => {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchInfiniteQuery(
-    postQueryKeys.postListInfinitePopularDateScope("All"),
-    () =>
-      api.postService.getPosts({
-        dateScope: "All",
-        sortScope: "likes",
-        page: 0,
-        maxContent: 10,
-      })
-  );
-  return {
-    props: {
-      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-    },
-    revalidate: 6000,
-  };
-};
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,19 +1,14 @@
-import { GetServerSideProps, NextPage } from "next";
+import { NextPage } from "next";
 
 import { searchSeo } from "library/seo/searchSeo";
 import { DefaultSeo } from "next-seo";
-import { dehydrate, QueryClient } from "react-query";
 
-import api from "@Api/index";
+import useStringRouter from "@Hooks/useStringRouter";
 import RootBox from "@Layouts/box/RootBox";
 import SearchPostSection from "@Models/search/SearchPostSection";
-import { postQueryKeys } from "@Utility/queryKey";
 
-const SearchPage: NextPage<{ categoryName: string }> = ({
-  categoryName,
-}: {
-  categoryName: string;
-}) => {
+const SearchPage: NextPage = () => {
+  const categoryName = useStringRouter("category");
   return (
     <div>
       <DefaultSeo {...searchSeo(categoryName)} />
@@ -22,29 +17,6 @@ const SearchPage: NextPage<{ categoryName: string }> = ({
       </RootBox>
     </div>
   );
-};
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { category } = context.query;
-  if (!category) return { props: {} };
-  if (Array.isArray(category)) return { props: {} };
-  const queryClient = new QueryClient();
-  await queryClient.prefetchInfiniteQuery(
-    postQueryKeys.postListInfiniteSearchCategoryName(category),
-    () =>
-      api.postService.getCategoryPosts({
-        dateScope: "All",
-        sortScope: "createdAt",
-        page: 0,
-        categoryName: category,
-        maxContent: 10,
-      })
-  );
-  return {
-    props: {
-      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-      categoryName: category,
-    },
-  };
 };
 
 export default SearchPage;
