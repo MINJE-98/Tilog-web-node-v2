718e4a0d3c5eaeddcb25e9d537a64a9ceb3bab2b
MINJE-98 <jmj012100@gmail.com>
2022-10-24 13:29:00 +0900

    feat: Paragraph에 fontSize props 추가

--- src/components/@commons/atom/paragraph/index.tsx
+++ src/components/@commons/atom/paragraph/index.tsx
@@ -1,6 +1,26 @@
-import tw from "twin.macro";
+import { HTMLAttributes } from "react";
 
-const Paragraph = tw.p`
-text-xs md:text-base text-neutral-500
+import tw, { styled } from "twin.macro";
+
+interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
+  fontSize: "large" | "medium" | "small";
+}
+
+const TextInputSizeStyle = ({ fontSize }: ParagraphProps) => {
+  switch (fontSize) {
+    case "large":
+      return tw`text-base font-semibold md:text-lg`;
+    case "medium":
+      return tw`text-sm font-medium md:text-base`;
+    case "small":
+      return tw`text-xs font-normal md:text-sm`;
+    default:
+      return "";
+  }
+};
+
+const Paragraph = styled.p`
+  ${tw`text-neutral-500`}
+  ${TextInputSizeStyle}
 `;
 export default Paragraph;
